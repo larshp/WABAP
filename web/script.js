6b260abb3d16deb07b3f6684d539081ac25eaa47
larshp <larshp@hotmail.com>
2016-03-26 16:14:52 +0000

    more

--- web/script.js
+++ web/script.js
@@ -1,18 +1,66 @@
-/* global CodeMirror */
-var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-  lineNumbers: true,
-  matchBrackets: true,
-  tabSize: 2,
-  theme: "seti",
-  mode: "abap"
-});
+var editor = null;
 
-editor.setOption("extraKeys", {
-  Tab: function(cm) {
-    var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
-    cm.replaceSelection(spaces);
+function toggleMode(cm) {
+  if(cm.getOption("readOnly") === true) {
+    cm.setOption("readOnly", false);
+    cm.setOption("theme", "seti");
+  } else {
+    cm.setOption("readOnly", true);
+    cm.setOption("theme", "bespin");
   }
-});
+}
+
+function initCodeMirror() {
+  var right = document.getElementsByClassName("floatright")[0];
+  right.innerHTML =
+    "<form>" +
+    "<textarea id=\"code\" name=\"code\"></textarea>" +
+    "</form>";
+
+  /* global CodeMirror */
+  editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    matchBrackets: true,
+    tabSize: 2,
+    theme: "seti",
+    mode: "abap"
+  });
+
+// todo, keys for commenting
+  editor.setOption("extraKeys", {
+    Tab: function(cm) {
+      var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+      cm.replaceSelection(spaces);
+    },
+    "Ctrl-F1": function(cm) {
+      toggleMode(cm);
+    },
+    "Ctrl-F2": function(cm) {
+      alert("todo, check!");
+    },
+    "Ctrl-F3": function(cm) {
+      alert("todo, activate!");
+    },
+    "Shift-F1": function(cm) {
+      alert("todo, pretty print");
+    },
+    "F11": function(cm) {
+      alert("todo, save");
+    },
+    "Ctrl-S": function(cm) {
+      alert("todo, save");
+    }
+  });
+}
+
+function openEditor() {
+  if(editor === null) {
+    initCodeMirror();
+  }
+
+  editor.focus();
+  editor.setValue( "* ABAP mode testing\nWRITE 'Hello world'.");
+}
 
 function rebuildMenu() {
   var left = document.getElementsByClassName("floatleft")[0];
@@ -60,7 +108,7 @@ document.onclick = function(e) {
   } else if(e.target.className === "list-content directory") {
     alert("todo, click directory");
   } else if(e.target.className === "list-content file") {
-    alert("todo, click file");
+    openEditor();
   } else if(e.target.className === "menu-item") {
     switch(e.target.innerText) {
       case "Rebuild menu":
