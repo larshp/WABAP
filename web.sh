720fdae123e625de1bb39cfa9e9ba16ba04c933f
larshp <larshp@hotmail.com>
2016-05-10 11:51:39 +0000

    more build stuff

---
+++ web.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp node_modules/codemirror/lib/codemirror.css web/
+cp node_modules/codemirror/theme/seti.css web/
+cp node_modules/codemirror/theme/bespin.css web/
+cp node_modules/codemirror/addon/dialog/dialog.css web/
+cp node_modules/octicons/octicons/octicons.css web/
+cp node_modules/codemirror/lib/codemirror.js web/
+cp node_modules/codemirror/addon/dialog/dialog.js web/
+cp node_modules/codemirror/addon/search/searchcursor.js web/
+cp node_modules/codemirror/addon/search/search.js web/
+cp node_modules/codemirror-abap/build/abap.js web/
+cp node_modules/octicons/octicons/octicons.ttf web/
